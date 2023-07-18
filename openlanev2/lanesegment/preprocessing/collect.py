7dab7c6f9ddbf2b993b19ba813ceb9cd77eb2dd1
Peggy <60730828+PeggyPeppa@users.noreply.github.com>
2023-07-18 16:58:44 +0800

    Update collect.py

--- openlanev2/lanesegment/preprocessing/collect.py
+++ openlanev2/lanesegment/preprocessing/collect.py
@@ -100,3 +100,82 @@ def collect(root_path : str, data_dict : dict, collection : str, point_interval 
                     meta[identifier]['sensor']['sd_map'].append(line)
 
     io.pickle_dump(f'{root_path}/{collection}.pkl', meta)
+
+def collect_lanesegment(root_path : str, data_dict : dict, collection : str, n_points : int = 10, with_sd_map : bool = False) -> None:
+    r"""
+    Load meta data of data in data_dict,
+    and store in a .pkl with split as file name.
+
+    Parameters
+    ----------
+    root_path : str
+    data_dict : dict
+        A dict contains ids of data to be preprocessed.
+    collection : str
+        Name of the collection.
+    point_interval : int
+        Interval for subsampling points of lane centerlines,
+        not subsampling as default.
+    with_sd_map : bool
+        Whether include SD Map for the SD Map as Prior Expansion.
+
+    """
+    data_list = [(split, segment_id, timestamp.split('.')[0]) \
+        for split, segment_ids in data_dict.items() \
+            for segment_id, timestamps in segment_ids.items() \
+                for timestamp in timestamps
+    ]
+    meta = {
+        (split, segment_id, timestamp): io.json_load(f'{root_path}/{split}/{segment_id}/info/{timestamp}-ls.json') \
+            for split, segment_id, timestamp in tqdm(data_list, desc=f'collecting {collection}', ncols=80)
+    }
+
+    for identifier, frame in meta.items():
+        for k, v in meta[identifier]['pose'].items():
+            meta[identifier]['pose'][k] = np.array(v, dtype=np.float64)
+        for camera in meta[identifier]['sensor'].keys():
+            for para in ['intrinsic', 'extrinsic']:
+                for k, v in meta[identifier]['sensor'][camera][para].items():
+                    meta[identifier]['sensor'][camera][para][k] = np.array(v, dtype=np.float64)
+
+        if 'annotation' not in frame:
+            continue
+        for i, area in enumerate(frame['annotation']['area']):
+            meta[identifier]['annotation']['area'][i]['points'] = fix_pts_interpolate( np.array(area['points']), n_points)
+        for i, lane_segment in enumerate(frame['annotation']['lane_segment']):
+            meta[identifier]['annotation']['lane_segment'][i]['centerline'] = fix_pts_interpolate( np.array(area['points']), n_points)
+            meta[identifier]['annotation']['lane_segment'][i]['left_laneline'] = fix_pts_interpolate( np.array(lane_segment['left_laneline']), n_points)
+            meta[identifier]['annotation']['lane_segment'][i]['right_laneline'] = fix_pts_interpolate( np.array(lane_segment['right_laneline']), n_points)
+        for i, traffic_element in enumerate(frame['annotation']['traffic_element']):
+            meta[identifier]['annotation']['traffic_element'][i]['points'] = np.array(traffic_element['points'], dtype=np.float32)
+        meta[identifier]['annotation']['topology_lsls'] = np.array(meta[identifier]['annotation']['topology_lsls'], dtype=np.int8)
+        meta[identifier]['annotation']['topology_lste'] = np.array(meta[identifier]['annotation']['topology_lste'], dtype=np.int8)
+
+        if with_sd_map:
+            split, segment_id, timestamp = identifier
+            sd_map = io.json_load(f'{root_path}/{split}/{segment_id}/sdmap.json')
+
+            translation = meta[identifier]['pose']['translation'][:2]
+            rotation = meta[identifier]['pose']['rotation'][:2, :2]
+
+            meta[identifier]['sensor']['sd_map'] = []
+            for road in sd_map:
+                road = (road - translation) @ rotation
+                road = LineString(road).intersection(box(*SD_MAP_RANGE))
+                if road.geom_type == 'MultiLineString':
+                    for new_pts_single in road.geoms:
+                        if new_pts_single.is_empty:
+                            continue
+                        line = np.array(new_pts_single.coords, dtype=np.float32)
+                        meta[identifier]['sensor']['sd_map'].append(line)
+                elif not road.is_empty:
+                    line = np.array(road.coords, dtype=np.float32)
+                    meta[identifier]['sensor']['sd_map'].append(line)
+
+    io.pickle_dump(f'{root_path}/{collection}.pkl', meta)
+
+def fix_pts_interpolate(lane, n_points):
+    ls = LineString(lane)
+    distances = np.linspace(0, ls.length, n_points)
+    lane = np.array([ls.interpolate(distance).coords[0] for distance in distances], dtype=np.float32)
+    return lane
